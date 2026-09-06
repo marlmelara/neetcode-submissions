83fe9fb1254c0d9c10c829e4aa418b658e834953
marlmelara <121407362+marlmelara@users.noreply.github.com>
2026-09-06 15:59:56 -0500

    Add: python-list-concat - submission-0

---
+++ Python Coding Interviews/python-list-concat/submission-0.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+def combine_elements(arr1: List[int], arr2: List[int]) -> List[int]:
+    combined_elements = arr1 + arr2
+
+    return combined_elements
+
+
+
+# do not modify below this line
+arr1 = [1, 3, 5]
+arr2 = [4, 6, 8]
+
+print(combine_elements(arr1, arr2))
+print(arr1)
+print(arr2)
